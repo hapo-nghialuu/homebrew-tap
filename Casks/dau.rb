15aff2b9fce8e30891723ece0a464d8c568f3491
hapo-nghialuu <nghialt@haposoft.com>
2026-08-08 16:47:33 +0700

    chore: bump dau to 0.1.7

--- Casks/dau.rb
+++ Casks/dau.rb
@@ -1,6 +1,6 @@
 cask "dau" do
-  version "0.1.6"
-  sha256 "97edd853288ecb604115e04ebb626c417644b15ab519de7d64a761087a64e48d"
+  version "0.1.7"
+  sha256 "c33c6eb5db634f8ae8d2179c92c812613df475b3813d4037fc6634215b9dfd8f"
 
   url "https://github.com/hapo-nghialuu/dau/releases/download/v#{version}/Dau-#{version}.zip"
   name "Dấu"
